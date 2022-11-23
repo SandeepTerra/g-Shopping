69e085ecc796ca533f4cfb9a6d0d549654303206
SandeepTerra <Sandeep@terra.group>
2022-11-23 13:11:29 +0200

    first commit

---
+++ mygshop.js
@@ -0,0 +1,49 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const { stringify } = require("querystring");
+
+const products = ["Rolex", "Nike", "GUCCI"];
+let results = [];
+
+
+const scrape = async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+
+    const page = await browser.newPage();
+
+    for (let i = 0; i < products.length; i++) {
+      await page.goto("https://shopping.google.co.za/");
+      await page.type(
+        "input[placeholder='What are you looking for?']",
+        products[i]
+      );
+      await page.click("button[aria-label='Google Search']");
+      await page.waitForSelector("#pnnext");
+
+      const shopurl = await page.$$eval("div.sh-dgr__grid-result a.xCpuod", (nodes) =>
+        nodes.map((n) => n.getAttribute("href"))
+      );
+
+      const shopurl1 = await page.$$eval("div.sh-dgr__grid-result a.shntl", (nodes) =>
+        nodes.map((n) => n.getAttribute("href"))
+      );
+
+      const pat = /^https?:\/\//i;
+      let firsturl = shopurl[0];
+      if (!pat.test(firsturl)) {
+        firsturl = "https://www.google.co.za" + firsturl;
+      }
+
+      results.push({ name: products[i], url: firsturl });
+      
+    }
+
+    //await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+scrape();
+
